23a36474594e098f0498df9de5c185efb11b6dad
Chuy <chuymaster3@hotmail.com>
2021-02-18 21:53:13 +0900

    Create project

---
+++ vtdashboard/vtdashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  vtdashboardApp.swift
+//  vtdashboard
+//
+//  Created by CHATCHAI LOKNIYOM on 2021/02/18.
+//
+
+import SwiftUI
+
+@main
+struct vtdashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
