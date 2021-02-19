8bb37455492514900d9c462bfacd860b61f5cc02
Chuy <chuymaster3@hotmail.com>
2021-02-19 13:11:08 +0900

    Implement Mock data

--- vtdashboard/Entities/Channel.swift
+++ vtdashboard/Entities/Channel.swift
@@ -14,20 +14,3 @@ struct Channel: Identifiable, Codable {
         return dateFormatter.string(from: Date(timeIntervalSince1970: updatedAt))
     }()
 }
-
-let dummyChannels: [Channel] = [
-    .init(
-        channelId: "UC7iCSRt7Jej2XE0MaM9gPgg",
-        title: "YuChan Channel",
-        thumbnailImageUrl: "https://yt3.ggpht.com/ytc/AAUvwngABpVP2Dh5kziMwBubM3LoBbn9G813luZ-1HqS=s240-c-k-c0x00ffffff-no-rj",
-        type: .original,
-        updatedAt: 1613646011196
-    ),
-    .init(
-        channelId: "UC3HM8L5j53x0WsliGNTXhbQ",
-        title: "Shanaru Ch.",
-        thumbnailImageUrl: "https://yt3.ggpht.com/ytc/AAUvwngV0-gmlgzNNWds0My5lISTZ5Bh3kk-5_iYehd0Sw=s240-c-k-c0x00ffffff-no-rj",
-        type: .half,
-        updatedAt: 1613646011189
-    )
-]
--- vtdashboard/Infrastructures/MockNetworkClient.swift
+++ vtdashboard/Infrastructures/MockNetworkClient.swift
@@ -1,10 +1,29 @@
 import Combine
 
+// swiftlint:disable force_cast
 class MockNetworkClient: NetworkClientProtocol {
     func get<T: Codable>(endpoint: Endpoint) -> Future<T, Error> {
         switch endpoint {
         case .getChannelList:
-            fatalError("Not implemented")
+            return Future { promise in
+                let channels: [Channel] = [
+                    .init(
+                        channelId: "UC7iCSRt7Jej2XE0MaM9gPgg",
+                        title: "YuChan Channel",
+                        thumbnailImageUrl: "https://yt3.ggpht.com/ytc/AAUvwngABpVP2Dh5kziMwBubM3LoBbn9G813luZ-1HqS=s240-c-k-c0x00ffffff-no-rj",
+                        type: .original,
+                        updatedAt: 1613646011196
+                    ),
+                    .init(
+                        channelId: "UC3HM8L5j53x0WsliGNTXhbQ",
+                        title: "Shanaru Ch.",
+                        thumbnailImageUrl: "https://yt3.ggpht.com/ytc/AAUvwngV0-gmlgzNNWds0My5lISTZ5Bh3kk-5_iYehd0Sw=s240-c-k-c0x00ffffff-no-rj",
+                        type: .half,
+                        updatedAt: 1613646011189
+                    )
+                ]
+                promise(.success(channels as! T))
+            }
         @unknown default:
             fatalError("URL Not found")
         }
