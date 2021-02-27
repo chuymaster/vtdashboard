4e50e2fa0c10ee66b81ff49aaa80a554dc5254a9
Chuy <chuymaster3@hotmail.com>
2021-02-27 10:16:11 +0900

    Checkin MainView

---
+++ vtdashboard/Application/MainView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct MainView: View {
+    
+    @State var currentViewType: ViewType = .channelRequests
+    
+    var body: some View {
+        HStack {
+            SidebarView(viewType: $currentViewType)
+                .frame(width: 220)
+            contentView
+        }
+        
+    }
+    
+    private var contentView: some View {
+        switch currentViewType {
+        case .channels:
+            return ChannelsView(
+                viewModel: ChannelsViewModel(
+                    networkClient: MockNetworkClient()
+                )
+            )
+            .eraseToAnyView()
+        case .channelRequests:
+            return ChannelRequestsView(
+                viewModel: ChannelRequestsViewModel(
+                    networkClient: MockNetworkClient()
+                )
+            )
+            .eraseToAnyView()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+            .previewLayout(.sizeThatFits)
+    }
+}
