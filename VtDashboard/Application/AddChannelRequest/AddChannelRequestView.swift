af0a47094556f6d7224aef677ee276e4098327a8
Chuy <chuymaster3@hotmail.com>
2021-02-27 22:07:53 +0900

    Add dummy AddChannelRequestView

---
+++ VtDashboard/Application/AddChannelRequest/AddChannelRequestView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AddChannelRequestView: View {
+    var body: some View {
+        Text("AddChannelRequestView!")
+    }
+}
+
+struct AddChannelRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddChannelRequestView()
+    }
+}
