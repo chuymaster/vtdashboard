2257477ccd7b75500da5c50b8e4fedd8912ca6d3
Chuy <chuymaster3@hotmail.com>
2021-03-01 00:09:16 +0900

    Adjust layout

--- VtDashboard/Application/AddChannelRequest/AddChannelRequestView.swift
+++ VtDashboard/Application/AddChannelRequest/AddChannelRequestView.swift
@@ -5,25 +5,56 @@ struct AddChannelRequestView: View {
     @State private var channelType = ChannelType.original
     @StateObject var viewModel = AddChannelRequestViewModel()
     
+    private let maxCharacterCount = 24
+    
     var body: some View {
-        VStack {
-            Text("Request new channel")
-                .font(.title2)
-            TextField("Enter Channel ID", text: $channelId)
-                .font(.body)
-                .padding()
+        VStack(spacing: 16) {
+            VStack {
+                TextField("Enter Channel ID", text: $channelId)
+                    .font(.headline)
+                if isAbleToSubmit {
+                    Text("Channel ID must begin with UC, must be exact 24 characters.")
+                        .font(.callout)
+                        .foregroundColor(.red)
+                }
+            }
+            .padding()
+            
             ChannelTypePicker(channelType: $channelType)
+            
             Button(action: {
-                viewModel.postChannelRequest(id: channelId, type: .original)
+                viewModel.postChannelRequest(
+                    id: channelId,
+                    type: .original
+                )
             }) {
                 Text("Submit")
             }
+            .keyboardShortcut(.defaultAction)
+            .disabled(isAbleToSubmit)
+            
+            // TODO:- get random youtube channel info to show
+            
             Spacer()
         }
         .padding()
         .alert(item: $viewModel.channelRequest) { channelRequest in
-            Alert(title: Text("Submission Completed"), message: Text("\(channelRequest.title)\nType: \(channelRequest.type.displayText)"), dismissButton: .default(Text("Close")))
+            Alert(
+                title: Text("Submission Completed"),
+                message: Text("\(channelRequest.title)\nType: \(channelRequest.type.displayText)"),
+                dismissButton: .default(Text("Close")
+                )
+            )
         }
+        .onChange(of: channelId) { value in
+            if channelId.count > maxCharacterCount {
+                channelId = String(channelId.prefix(maxCharacterCount))
+            }
+        }
+    }
+    
+    private var isAbleToSubmit: Bool {
+        channelId.prefix(2) != "UC" || channelId.count != maxCharacterCount
     }
 }
 
