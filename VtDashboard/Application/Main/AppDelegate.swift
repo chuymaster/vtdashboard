1c061fdbe3604f0a4562e2e91d8b6a63cd4f2c0a
Chuy <chuymaster3@hotmail.com>
2021-04-05 00:27:52 +0900

    Make app a menu bar app

---
+++ VtDashboard/Application/Main/AppDelegate.swift
@@ -0,0 +1,35 @@
+import Foundation
+import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    private var menuBarItem: NSStatusItem!
+    private lazy var popover: NSPopover = {
+        let mainView = MainView()
+            .environmentObject(UIState.shared)
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 800, height: 600)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: mainView)
+        return popover
+    }()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        menuBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = menuBarItem.button {
+            button.image = NSImage(named: "MenuBarIcon")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+    
+    // Create the status item
+    @objc private func togglePopover(_ sender: AnyObject?) {
+        if let button = menuBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+}
--- VtDashboard/Application/Main/VtDashboardApp.swift
+++ VtDashboard/Application/Main/VtDashboardApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct VtDashboardApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             MainView()
